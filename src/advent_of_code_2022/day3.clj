1784cbd6d34477285be5e00fdd7b15c95cfa3821
Jacob Hawk <hawk.music24@gmail.com>
2023-02-17 14:13:37 -0700

    day 3 done

--- src/advent_of_code_2022/day3.clj
+++ src/advent_of_code_2022/day3.clj
@@ -24,3 +24,34 @@
     (+ adder (str/index-of priorities (first (get-shared comp1 comp2))))))
 
 (reduce abc 0 (-> real-in str/split-lines))
+
+
+;;;;;; part 2 ;;;;;;
+
+(partition 3 (-> ez-in str/split-lines))
+(def group '("vJrwpWtwJgWrhcsFMMfFFhFp" "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL" "PmmdzqPrVvPwwTWBwg"))
+
+(defn get-group-badge-val [inc group-of-three]
+  (+ inc (->> group-of-three
+              (map set)
+              (apply set/intersection)
+              first
+              (str/index-of priorities))))
+
+(->> real-in
+      str/split-lines
+      (partition 3)
+      (reduce get-group-badge-val 0))
+
+
+(comment
+
+  (as-> group $
+        (map set $)
+        (apply set/intersection $)
+        (first $)
+        (str/index-of priorities $))
+
+  (str/index-of priorities \r)
+
+  )
