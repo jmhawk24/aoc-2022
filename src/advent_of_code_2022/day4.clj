3eccd1beffe90f64d6a7ada441864aed9c0e0a9f
Jacob Hawk <hawk.music24@gmail.com>
2023-02-17 22:17:38 -0700

    day 3 setup

---
+++ src/advent_of_code_2022/day4.clj
@@ -0,0 +1,7 @@
+(ns advent-of-code-2022.day4)
+
+;; if the size of the set of selections doesn't change after union,
+;; then one is contained by the other
+
+(def ez-in "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8")
+
